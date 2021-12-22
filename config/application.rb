0afa1fdf364d51a95752ed9236369d25f77953e8
Philipp Obrivanov <milliardex@gmail.com>
2021-12-23 00:45:47 +0300

    Set default locale and time zone

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,10 @@ module Millionaire
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
