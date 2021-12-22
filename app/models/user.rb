e673155ec74ca4f5880c0a0a4ae26eecc72c5f7e
Philipp Obrivanov <milliardex@gmail.com>
2021-12-23 00:27:18 +0300

    Add User to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  validates :name, presence: true
+
+  validates :is_admin, inclusion: {in: [true, false]}, allow_nil: false
+
+  validates :balance, numericality: {only_integer: true}, allow_nil: false
+
+  has_many :games, dependent: :destroy
+
+  def average_prize
+    (balance.to_f / games.count).round unless games.count.zero?
+  end
 end
