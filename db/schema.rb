7b75209b0604b05e1bdcc9c17a0dd5f93c63a8ee
Philipp Obrivanov <milliardex@gmail.com>
2021-12-23 21:54:11 +0300

    Add help hash to game_question migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_184750) do
+ActiveRecord::Schema.define(version: 2021_12_23_185122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_184750) do
     t.integer "d"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "help_hash"
     t.index ["game_id"], name: "index_game_questions_on_game_id"
     t.index ["question_id"], name: "index_game_questions_on_question_id"
   end
