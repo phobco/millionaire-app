ec860c8e911eb03948618e5f242675b29913ebcd
Philipp Obrivanov <milliardex@gmail.com>
2021-12-23 21:49:32 +0300

    Add help field to game migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_141658) do
+ActiveRecord::Schema.define(version: 2021_12_23_184750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2021_12_23_141658) do
     t.integer "prize", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "fifty_fifty_used", default: false, null: false
+    t.boolean "audience_help_used", default: false, null: false
+    t.boolean "friend_call_used", default: false, null: false
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
