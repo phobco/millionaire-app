b53515604e0de32815c18385df943cc431017172
Philipp Obrivanov <milliardex@gmail.com>
2021-12-25 14:24:00 +0300

    Add routes for games and questions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :users, only: %i[index show]
+
+  resources :games, only: %i[create show] do
+    put 'help', on: :member 
+    put 'answer', on: :member 
+    put 'take_money', on: :member
+  end
+
+  resource :questions, only: %i[new create]
 end
