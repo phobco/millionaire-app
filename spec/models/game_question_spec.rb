12de423b0c8ac549a75e726d82c8a8c1b0c65b53
Philipp Obrivanov <milliardex@gmail.com>
2021-12-30 21:57:34 +0300

    Add spec models

---
+++ spec/models/game_question_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe GameQuestion, type: :model do
+  let(:game_question) do
+    FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3, finished_at: nil)
+  end
+
+  # Группа тестов на игровое состояние объекта вопроса
+  context 'game status' do
+    # Тест на правильную генерацию хэша с вариантами
+    it 'correct .variants' do
+      expect(game_question.variants).to eq(
+        'a' => game_question.question.answer2,
+        'b' => game_question.question.answer1,
+        'c' => game_question.question.answer4,
+        'd' => game_question.question.answer3
+      )
+    end
+
+    it 'correct .answer_correct?' do
+      # Именно под буквой b в тесте мы спрятали указатель на верный ответ
+      expect(game_question.answer_correct?('b')).to be_truthy
+    end
+  end
+end
