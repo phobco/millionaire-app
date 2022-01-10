8ab19d78551abac71c34ca29333f62c206e0211f
Philipp Obrivanov <milliardex@gmail.com>
2022-01-10 21:29:25 +0300

    Add specs for helps

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -27,5 +27,42 @@ RSpec.describe GameQuestion, type: :model do
     it 'correct .correct_answer_key' do
       expect(game_question.correct_answer_key).to eq('b')
     end
+
+    it 'correct .help_hash' do
+      expect(game_question.help_hash).to eq({})
+
+      game_question.help_hash[:some_key1] = 'value1'
+      game_question.help_hash['some_key2'] = 'value2'
+
+      expect(game_question.save).to be(true)
+
+      gq = GameQuestion.find(game_question.id)
+
+      expect(gq.help_hash).to eq({some_key1: 'value1', 'some_key2' => 'value2'})
+    end
+  end
+
+  context 'user helpers' do
+    it 'correct audience_help' do
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+
+      ah = game_question.help_hash[:audience_help]
+      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+  end
+
+  it 'correct fifty_fifty' do
+    expect(game_question.help_hash).not_to include(:fifty_fifty)
+    game_question.add_fifty_fifty
+  
+    expect(game_question.help_hash).to include(:fifty_fifty)
+    ff = game_question.help_hash[:fifty_fifty]
+  
+    expect(ff).to include('b')
+    expect(ff.size).to eq(2)
   end
 end
