651a9e9b20d16e21920536cec1910317b24ee58c
Philipp Obrivanov <milliardex@gmail.com>
2022-01-10 19:21:20 +0300

    Add specs for anonymous user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -14,6 +14,38 @@ RSpec.describe GamesController , type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to be
     end
+
+    it 'kick from #create' do
+      post :create
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kick from #answer' do
+      put :answer, params: { id: 1, letter: 'a' }
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kick from #take_money' do
+      put :take_money, params: { id: 1 }
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)  
+      expect(flash[:alert]).to be
+    end
+
+    it 'kick from #help' do
+      put :help, params: { id: 1 }
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)  
+      expect(flash[:alert]).to be
+    end
   end
   
   context 'when user is logged in' do
