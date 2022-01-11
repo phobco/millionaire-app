923dce5164900c4895a4b3400ede4dfcca576c3a
Philipp Obrivanov <milliardex@gmail.com>
2022-01-11 19:40:51 +0300

    Add specs for show page

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  context 'when the user views their own page' do
+    before do
+      user = assign(:user, FactoryBot.build_stubbed(:user, name: 'User111'))
+      allow(view).to receive(:current_user).and_return(user)
+
+      render
+    end
+
+    it 'render user name' do
+      expect(rendered).to match('User111')
+    end
+
+    it 'render change name and password link' do
+      expect(rendered).to match('Сменить имя и пароль')
+    end
+
+    it 'render game partial' do
+      assign(:games, [FactoryBot.build_stubbed(:game, id: 5, created_at: 1.minute.ago, current_level: 5)])
+      stub_template 'users/_game.html.erb' => 'User game goes here'
+      render
+
+      expect(rendered).to match('User game goes here')
+    end
+  end
+  
+  context 'when user views someone else page' do
+    before do
+      assign(:user, FactoryBot.build_stubbed(:user, name: 'User222'))
+
+      render
+    end
+    
+    it 'render user name' do
+      expect(rendered).to match('User222')
+    end
+
+    it 'not render change name and password link' do
+      expect(rendered).not_to match('Сменить имя и пароль')
+    end
+
+    it 'render game partial' do
+      assign(:games, [FactoryBot.build_stubbed(:game, id: 7, created_at: 1.minute.ago, current_level: 7)])
+      stub_template 'users/_game.html.erb' => 'User game goes here'
+      render
+
+      expect(rendered).to match('User game goes here')
+    end
+  end
+end
