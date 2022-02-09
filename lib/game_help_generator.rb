2e065ddadb615e915f2781948ef1b9ebf16af268
Philipp Obrivanov <milliardex@gmail.com>
2022-02-09 18:33:16 +0300

    Translate comments to en

--- lib/game_help_generator.rb
+++ lib/game_help_generator.rb
@@ -1,9 +1,7 @@
 class GameHelpGenerator
-  # Сколько всего виртуальных зрителей в игре (в процентах получается)
   TOTAL_WATCHERS = 100
 
-  # Возвращает hash c массивом ключей keys и значениями — распределением в
-  # процентах.
+  # Returns a hash with an array of keys
   def self.audience_distribution(keys, correct_key)
     result_array = []
 
@@ -15,20 +13,17 @@ class GameHelpGenerator
       end
     end
 
-    # Нормализуем массив
     sum = result_array.sum
     result_array.map! { |v| TOTAL_WATCHERS * v / sum }
 
-    # Возвращаем хэш, собранный из массива ключей и значений
     Hash[keys.zip(result_array)]
   end
 
-  # Возвращает строку подсказки: что советует друг.
+  # Returns a hint string
   def self.friend_call(keys, correct_key)
-    # C ~80% вероятностью выбираем правильный ключ.
+    # Choose wright key with 80% probability.
     key = (rand(10) > 2) ? correct_key : keys.sample
 
-    # Генерируем подсказку от друга, используя файл локали
     I18n.t(
       'game_help.friend_call', variant: key.upcase,
       name: I18n.t('game_help.friends').sample
