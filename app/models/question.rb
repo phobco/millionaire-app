1590480b99d3e87dba7e68ad397568fea5f5ed72
Philipp Obrivanov <milliardex@gmail.com>
2022-02-11 19:36:39 +0300

    Refactor

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   QUESTION_LEVELS = (0..14).freeze
 
-  validates :level, presence: true, inclusion: {in: QUESTION_LEVELS}
+  validates :level, presence: true, inclusion: { in: QUESTION_LEVELS }
 
   validates :text, presence: true, uniqueness: true, allow_blank: false
 
