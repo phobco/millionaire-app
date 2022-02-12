d421b8ace5aec5a15c38aac9e809e59a4d118661
Philipp Obrivanov <milliardex@gmail.com>
2022-02-12 10:34:52 +0300

    Refactor config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,11 +101,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'millionaire-phobco.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              "smtp.gmail.com",
+    address:              'smtp.gmail.com',
     port:                 587,
     user_name:            Rails.application.credentials.dig(:mail, :email),
     password:             Rails.application.credentials.dig(:mail, :pass),
-    authentication:       "plain",
+    authentication:       'plain',
     enable_starttls_auto: true
   }
 end
