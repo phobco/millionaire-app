33b43b7f4c9d4760410031a7830d5dbe8b87ec24
Philipp Obrivanov <milliardex@gmail.com>
2022-02-12 16:13:13 +0300

    Сolored game statuses

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,9 +1,19 @@
 module GamesHelper
   def game_label(game)
+    status_styles =
+      case game.status
+      when :in_progress then 'bg-warning'
+      when :timeout     then 'bg-secondary'
+      when :fail        then 'bg-danger'
+      else                   'bg-success'
+      end
+    
     if game.status == :in_progress && current_user == game.user
-      link_to content_tag(:span, t("game_statuses.#{game.status}"), class: 'label'), game_path(game)
+      link_to content_tag(:span, t("game_statuses.#{game.status}")),
+                game_path(game),
+                class: "link-secondary text-decoration-none #{status_styles} rounded p-1"
     else
-      content_tag :span, t("game_statuses.#{game.status}"), class: 'label label-danger'
+      content_tag :span, t("game_statuses.#{game.status}"), class: "#{status_styles} rounded p-1"
     end
   end
 end
