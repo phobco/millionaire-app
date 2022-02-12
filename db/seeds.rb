514cf5a5cbc184144333d02d7b30583ca4459185
Philipp Obrivanov <milliardex@gmail.com>
2022-02-12 16:38:46 +0300

    Add seeds for demo app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.create(level: 0, text: 'Question 1?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 1, text: 'Question 2?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 2, text: 'Question 3?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 3, text: 'Question 4?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 4, text: 'Question 5?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 5, text: 'Question 6?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 6, text: 'Question 7?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 7, text: 'Question 8?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 8, text: 'Question 9?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 9, text: 'Question 10?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 10, text: 'Question 11?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 11, text: 'Question 12?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 12, text: 'Question 13?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 13, text: 'Question 14?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
+Question.create(level: 14, text: 'Question 15?', answer1: '42', answer2: '1', answer3: '2', answer4: '3')
